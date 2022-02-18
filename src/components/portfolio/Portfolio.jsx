78dc71b2ad6ac7d943f6a665f5f0e94f1461cfe3
Bekzhan <bidahmetov2002@gmail.com>
2022-02-18 17:17:32 +0100

    Portfolio Section without tabs implemented

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,35 @@ import "./portfolio.scss"
 export default function Portfolio() {
   return (
     <div className='portfolio' id="portfolio">
-
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Feautured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+        <li>Branding</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="https://figmatemplate.com/wp-content/uploads/2020/04/Mobile-Banking-App-Figma.jpg" alt="" />
+          <h3>Banking App</h3>
+        </div><div className="item">
+          <img src="https://figmatemplate.com/wp-content/uploads/2020/04/Mobile-Banking-App-Figma.jpg" alt="" />
+          <h3>Banking App</h3>
+        </div><div className="item">
+          <img src="https://figmatemplate.com/wp-content/uploads/2020/04/Mobile-Banking-App-Figma.jpg" alt="" />
+          <h3>Banking App</h3>
+        </div><div className="item">
+          <img src="https://figmatemplate.com/wp-content/uploads/2020/04/Mobile-Banking-App-Figma.jpg" alt="" />
+          <h3>Banking App</h3>
+        </div><div className="item">
+          <img src="https://figmatemplate.com/wp-content/uploads/2020/04/Mobile-Banking-App-Figma.jpg" alt="" />
+          <h3>Banking App</h3>
+        </div><div className="item">
+          <img src="https://figmatemplate.com/wp-content/uploads/2020/04/Mobile-Banking-App-Figma.jpg" alt="" />
+          <h3>Banking App</h3>
+        </div>
+      </div>
     </div>
   )
 }
