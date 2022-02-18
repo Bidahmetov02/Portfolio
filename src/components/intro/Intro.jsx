6123a8f796db9dc3601b21c13906a5e31e560fe7
Bekzhan <bidahmetov2002@gmail.com>
2022-02-18 03:49:03 +0100

    iTyped animation implemented

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,19 @@
 import "./intro.scss"
+import {init} from 'ityped'
+import { useEffect, useRef } from "react"
 
 export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(()=>{
+    init(textRef.current, {
+      showCursor: false,
+      backDelay: 1500,
+      showCursor: true,
+      strings: ["Developer", "Designer", "Content Creator"],
+    })
+  }, []);
+
   return (
     <div className='intro' id="intro">
       <div className="left">
@@ -12,7 +25,7 @@ export default function Intro() {
         <div className="wrapper">
           <h2>Hi There, I'm</h2>
           <h1>Bezhan Bidakhmetov</h1>
-          <h3>Freelance<span></span></h3>
+          <h3>Freelance <span ref={textRef}></span></h3>
         </div>
         <a href="#portfolio">
           <img src="assets/down.png" alt="" />
