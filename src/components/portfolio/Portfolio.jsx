0a090d489b4ce07d6617746f0382f56f013c102a
Bekzhan <bidahmetov2002@gmail.com>
2022-02-19 04:00:00 +0100

    Dynamic active class implemented

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,15 +1,42 @@
+import { ListItem } from "@material-ui/core";
+import { useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss"
 
 export default function Portfolio() {
+  const [selected, setSelected] = useState('featured');
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "mobile",
+      title: "Mobile App",
+    },
+    {
+      id: "design",
+      title: "Design",
+    },
+    {
+      id: "content",
+      title: "Content",
+    },
+  ];
+
   return (
     <div className='portfolio' id="portfolio">
       <h1>Portfolio</h1>
       <ul>
-        <li className="active">Feautured</li>
-        <li>Web App</li>
-        <li>Mobile App</li>
-        <li>Design</li>
-        <li>Branding</li>
+        {list.map(i=>(
+          <PortfolioList title={i.title} active={selected === i.id} 
+            setSelected={setSelected} id={i.id}
+          />
+        ))}
       </ul>
       <div className="container">
         <div className="item">
