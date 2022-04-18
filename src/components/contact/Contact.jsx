6f971845bbfe7117332b4946272f80ff323100c3
Bekzhan <bidahmetov2002@gmail.com>
2022-04-18 13:53:41 +0200

    Contact send message implemented using EmailJS package

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,17 +1,33 @@
 import "./contact.scss" 
+import { useState } from "react";
 import emailjs from '@emailjs/browser';
 import emailKeys from '../../emailKeys'
+import validator from 'validator'
 
 export default function Contact() {
 
+  const [message, setMessage] = useState('')
+
   const handleSubmit = (e) => {
     e.preventDefault()
+    let email = e.target.elements.email.value
 
+    if (validator.isEmail(email)) {
+      sendEmail()
+      setMessage('Message sent succesfully! I will reply ASAP :)')
+      e.target.elements.email.value = ''
+      e.target.elements.message.value = ''
+    } else {
+      setMessage('Enter a valid Email Adress!')
+    }
+  }
+
+  const sendEmail = () => {
     emailjs.sendForm(emailKeys.SERVICE_ID, emailKeys.TEMPLATE_ID, "#form", emailKeys.PUBLIC_KEY)
       .then((res) => {
         console.log(res.text)
       }, (err) => {
-        console.log(err.text)
+        console.log(err)
       })
   }
 
@@ -23,8 +39,9 @@ export default function Contact() {
       <div className="right">
         <h2>Contact Me.</h2>
         <form onSubmit={handleSubmit} id="form">
-          <input type="email" placeholder="Email" name="email" required/>
+          <input type="text" placeholder="Email" name="email" required/>
           <textarea placeholder="Message" name="message" required></textarea>
+          <span className={message.includes("succesfully!") ? "valid" : "notValid"}>{message}</span>
           <button type="submit">Send</button>
         </form>
       </div>
