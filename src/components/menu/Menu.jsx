eeb4f2172cfde06fd9e8d45b3ec7077709b60f6e
Bekzhan <bidahmetov2002@gmail.com>
2022-05-03 13:20:50 +0200

    First mobile version done

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,4 +1,5 @@
 import "./menu.scss"
+import {Person, Mail} from "@material-ui/icons"
 
 export default function menu({menuOpen, SetMenuOpen}) {
   return (
@@ -10,6 +11,19 @@ export default function menu({menuOpen, SetMenuOpen}) {
             <li onClick={()=>SetMenuOpen(false)}><a href="#skills">Skills</a></li>
             <li onClick={()=>SetMenuOpen(false)}><a href="#contact">Contact</a></li>
         </ul>
+        <div className="MenuContact">
+            <div className="MenuitemContainer">
+              <Person className="icon" />
+              <span>+48 660 449 874</span>
+            </div>
+            <div className="MenuitemContainer">
+              <Mail className="icon" />
+              <span>bidahmetov2002@gmail.com</span>
+            </div>
+            <div className="MenuitemContainer">
+              <span><a target="_blank" href="https://drive.google.com/file/d/1MO0AhRIKzHA7O_0Vqs71wEf9JGTtGLPt/view?usp=sharing">Resume</a></span>
+            </div>
+        </div>
     </div>
   )
 }
